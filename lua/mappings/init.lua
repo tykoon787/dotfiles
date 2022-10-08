3b444cae41a8dd028383bd9ed02b7361403abf95
tykoon787 <tykoon787@protonmail.com>
2022-10-08 16:44:15 +0300

    Made some mapping changes

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -72,3 +72,5 @@ keymap("n", "<C-t>", ":ToggleTerm<CR>", opts)
 -- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+--
+
